c30f0f9a83fdc4a021b1a3fc0d42d7428aac5ecc
Ryan Atkinson <mail@ryanatkn.com>
2021-05-10 15:56:01 -0600

    hack build task

---
+++ src/build.task.ts
@@ -0,0 +1,12 @@
+import type {Task} from '@feltcoop/gro';
+
+export const task: Task = {
+	description: 'build the project',
+	dev: false,
+	run: async ({invokeTask, fs}): Promise<void> => {
+		await invokeTask('gro/build');
+		// TODO replace with new build system in gro@0.22
+		await fs.remove('dist');
+		await fs.copy('.gro/prod/lib', 'dist');
+	},
+};
